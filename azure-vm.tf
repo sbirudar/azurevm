d12a26959bd907b87f02bffb45b6e6edc92575a0
sbirudar <52947502+sbirudar@users.noreply.github.com>
2019-09-21 16:01:45 +0530

    Update azure-vm.tf

--- azure-vm.tf
+++ azure-vm.tf
@@ -1,3 +1,29 @@
+
+
+variable "sid" {
+  default = "groupc"
+}
+
+variable "cid" {
+  default = "groupc"
+}
+
+variable "cs" {
+  default = "groupc"
+}
+
+
+variable "tid" {
+  default = "groupc"
+}
+
+provider "azurerm" {
+    subscription_id = "${var.sid}"
+    client_id       = "${var.cid}"
+    client_secret   = "${var.cs}"
+    tenant_id       = "${var.tid}"
+}
+
 variable "prefix" {
   default = "groupc"
 }
